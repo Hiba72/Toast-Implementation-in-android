5832feb557f2154ccc56d84f99face75dec12c39
Hiba Saqib <hibasaqib72@gmail.com>
2021-11-14 16:47:59 +0500

    Simple toast implemented

---
+++ app/src/main/java/com/example/toastimplementation/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.toastimplementation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast toast=Toast.makeText(this,"Toast text",Toast.LENGTH_LONG);
+        toast.show();
+    }
+}
